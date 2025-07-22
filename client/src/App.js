600114a1e284687ad17e4a5e1b6cda46fde1aa97
Your Name <your.email@example.com>
2025-07-22 22:39:52 +0530

    Fix ESLint warnings by removing unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
